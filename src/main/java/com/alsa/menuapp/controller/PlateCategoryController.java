fd8d796e173ed4865a35db0b19ca66d318c8d68b
aldo1411 <62414829+aldo1411@users.noreply.github.com>
2022-11-24 09:34:18 -0600

    delete plate category controller added

--- src/main/java/com/alsa/menuapp/controller/PlateCategoryController.java
+++ src/main/java/com/alsa/menuapp/controller/PlateCategoryController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Description;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,21 +33,28 @@ public class PlateCategoryController {
     }
 
     @GetMapping("/{id}")
-    @Description(value = "returns order by its id")
+    @Description(value = "returns plate category by its id")
     public ResponseEntity<PlateCategory> getBill(@PathVariable int id) {
       return ResponseEntity.ok().body(cService.getPlateCategory(id));
     }
 
     @PostMapping()
-    @Description(value = "saves a new order given a order by the body")
+    @Description(value = "saves a new plate category given a plate category by the body")
     public ResponseEntity<PlateCategory> saveBill(@RequestBody PlateCategory plateCategory) {
       URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/restaurant/plateCategories").toUriString());
       return ResponseEntity.created(uri).body(cService.savePlateCategory(plateCategory));
     }
 
     @PutMapping("/{id}")
-    @Description(value = "updates order given a order by the body")
+    @Description(value = "updates plate category given a plate category by the body")
     public ResponseEntity<PlateCategory> updateBill(@PathVariable int id, @RequestBody Map<Object, Object> fields) {
         return ResponseEntity.ok().body(cService.updatePlateCategory(id, fields));
+    }
+
+    @DeleteMapping("/{id}")
+    @Description(value = "deletes a plate category given an id by path variable")
+    public ResponseEntity<Integer> deletePlate(@PathVariable int id) {
+        cService.deletePlateCategory(id);
+        return ResponseEntity.ok().body(id);
     }
 }
